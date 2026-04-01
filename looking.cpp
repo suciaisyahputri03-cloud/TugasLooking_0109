866a9e5a1c92fd5ce7d325332810faf7e427b254
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-04-01 09:09:52 +0800

    Menambahkan function menu untuk menampilkan pilihan

--- looking.cpp
+++ looking.cpp
@@ -9,3 +9,12 @@ void inputAngka(){
     cout<<"masukkan bilangan : ";
     cin>>n;
 }
+
+int menu(){
+    cout<<"===== MENU ====="<<endl;
+    cout<<"1. Cek Bilangan Prima"<<endl;
+    cout<<"2. Cek Bilangan Fibonacci"<<endl;
+    cout<<"0. Keluar"<<endl;
+    cout<<"Pilih menu : ";
+    cin>>pilihan;
+}
